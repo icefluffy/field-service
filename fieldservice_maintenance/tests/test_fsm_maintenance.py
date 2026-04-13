dc40aec3ba5721ce076b9ec67179017006e0654b
icefluffy <icefluffy@users.noreply.github.com>
2026-04-13 16:40:59 +0200

    Refactor FSM maintenance tests for clarity and structure

--- fieldservice_maintenance/tests/test_fsm_maintenance.py
+++ fieldservice_maintenance/tests/test_fsm_maintenance.py
@@ -1,41 +1,45 @@
 # Copyright (C) 2020, Brian McMaster
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
 
 
+@tagged("post_install", "-at_install")
 class TestFSMMaintenance(TransactionCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env["res.partner"].create({"name": "Partner"})
+        cls.fsm_loc = cls.env["fsm.location"].create(
+            {"name": "Test Maintenance Location", "owner_id": cls.partner.id}
+        )
+
     def test_fsm_maintenance(self):
-        # Create FSM Location to use in test cases
-        partner = self.env["res.partner"].create(
-            {
-                "name": "Partner",
-            }
-        )
-        fsm_loc = self.env["fsm.location"].create(
-            {"name": "Test Maintenance Location", "owner_id": partner.id}
-        )
         # Create FSM equipment
         fsm_equip_01 = self.env["fsm.equipment"].create(
             {
                 "name": "Test FSM Equipment 01",
-                "current_location_id": fsm_loc.id,
+                "current_location_id": self.fsm_loc.id,
             }
         )
         maint_equip_01 = fsm_equip_01.maintenance_equipment_id
         self.assertTrue(maint_equip_01)
+
         # Create FSM Order of type maintenance and an equipment. This should
         # create a maintenance request based on the FSM order
         fsm_order_01 = self.env["fsm.order"].create(
             {
-                "location_id": fsm_loc.id,
+                "location_id": self.fsm_loc.id,
                 "type": self.env.ref(
                     "fieldservice_maintenance.fsm_order_type_maintenance"
                 ).id,
                 "equipment_id": fsm_equip_01.id,
             }
         )
-        # Verify Maintenance order was created for that equipment
+
+        # Verify Maintenance request was created for that equipment
         maint_req_01 = self.env["maintenance.request"].search(
             [("fsm_order_id", "=", fsm_order_01.id)]
         )
@@ -47,13 +51,14 @@ class TestFSMMaintenance(TransactionCase):
         self.assertEqual(fsm_order_01.request_id, maint_req_01)
 
         # Create a maintenance request for an FSM equipment. This should
-        # create FSM order for that equipment.
+        # create an FSM order for that equipment.
         maint_req_02 = self.env["maintenance.request"].create(
             {
                 "name": "Equip 01 Request for FSM",
                 "equipment_id": maint_equip_01.id,
             }
         )
+
         # Test order was created for the request
         fsm_order_02 = self.env["fsm.order"].search(
             [("request_id", "=", maint_req_02.id)]
@@ -73,10 +78,16 @@ class TestFSMMaintenance(TransactionCase):
                 "equipment_id": maint_equip_01.id,
             }
         )
-        # and check that a notification regarding its missing value
+
+        # Check that a notification regarding the missing location
         # is shown to the user
         msg_notification = self.env["mail.message"].search(
-            [("res_id", "=", request.id)], order="id desc", limit=1
+            [
+                ("res_id", "=", request.id),
+                ("model", "=", "maintenance.request"),
+            ],
+            order="id desc",
+            limit=1,
         )
         self.assertRegex(
             msg_notification.body,
@@ -85,5 +96,5 @@ class TestFSMMaintenance(TransactionCase):
 
         # Deleting the FSM Equipment
         fsm_equip_01.unlink()
-        # Verfiy the maintenance equipment is no longer a FSM equipment
+        # Verify the maintenance equipment is no longer an FSM equipment
         self.assertFalse(maint_equip_01.is_fsm_equipment)
