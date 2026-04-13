42575b9f0fdaa723270d7a36c547c10dcb5d52fb
icefluffy <icefluffy@users.noreply.github.com>
2026-04-13 16:19:17 +0200

    Added FS Maint v14

---
+++ fieldservice_maintenance/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Field Service - Maintenance",
+    "summary": "Integrate Field Service orders with maintenance requests",
+    "version": "18.0.1.0.2",
+    "category": "Field Service",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/icefluffy/field-service",
+    "depends": [
+        "fieldservice",
+        "maintenance",
+    ],
+    "data": [
+        "data/fsm_order_type.xml",
+        "views/maintenance_equipment_view.xml",
+        "views/maintenance_request_view.xml",
+        "views/fsm_equipment_view.xml",
+        "views/fsm_order_view.xml",
+    ],
+    "pre_init_hook": "pre_init_hook",
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": [
+        "Sebastien",
+        "seth",
+    ],
+    "installable": True,
+}
