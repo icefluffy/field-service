1a2346eeb35988757d57d975f70071808783ba8a
icefluffy <icefluffy@users.noreply.github.com>
2026-04-21 15:47:28 +0200

    Refactor _compute_full_name method in FSMCategory

--- fieldservice/models/fsm_category.py
+++ fieldservice/models/fsm_category.py
@@ -1,7 +1,4 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FSMCategory(models.Model):
@@ -23,10 +20,11 @@ class FSMCategory(models.Model):
 
     _sql_constraints = [("name_uniq", "unique (name)", "Category name already exists!")]
 
+    @api.depends("name", "parent_id", "parent_id.full_name")
     def _compute_full_name(self):
         for record in self:
-            record.full_name = (
-                record.parent_id.full_name + "/" + record.name
-                if record.parent_id
-                else record.name
-            )
+            parent = record.parent_id
+            if parent and parent.full_name:
+                record.full_name = parent.full_name + "/" + record.name
+            else:
+                record.full_name = record.name or ""
