7369e15750ff387ac1ba3e678e1c454e6a73f1a9
icefluffy <icewitty@gmail.com>
2026-07-28 20:59:19 +0200

    huh

--- fieldservice_stock_account/__manifest__.py
+++ fieldservice_stock_account/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Field Service - Stock Account",
-    "version": "14.0.1.0.1",
+    "version": "18.0.1.0.1",
     "category": "Field Service",
     "summary": "Invoice inventory items delivered with Field Service orders",
     "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
