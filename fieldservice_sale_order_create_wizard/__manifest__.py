cdb71a7c4f4a9e0295b26ff0aaef9b9399213498
icefluffy <icewitty@gmail.com>
2026-07-28 20:58:58 +0200

    v18

--- fieldservice_sale_order_create_wizard/__manifest__.py
+++ fieldservice_sale_order_create_wizard/__manifest__.py
@@ -1,14 +1,12 @@
-# Copyright 2025 Bernat Obrador (APSL-Nagarro)<bobrador@apsl.net>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
+# -*- coding: utf-8 -*-
 {
     "name": "Field Service - Sale Order Create Wizard",
-    "version": "15.0.1.1.0",
+    "version": "18.0.1.0.0",
     "summary": "Create Sale Order from Field Service Kanban View",
     "category": "Field Service",
-    "website": "https://github.com/OCA/field-service",
-    "author": "APSL-Nagarro, Odoo Community Association (OCA)",
-    "maintainers": ["BernatObrador"],
+    "website": "https://github.com/icefluffy/field-service",
+    "author": "APSL-Nagarro, Odoo Community Association (OCA) adapted by IceFLuffy",
+    "maintainers": ["nobody", "icefluffy"],
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -19,12 +17,4 @@
         "security/ir.model.access.csv",
         "wizard/fsm_create_so_wizard.xml",
     ],
-    "assets": {
-        "web.assets_backend": [
-            "fieldservice_sale_order_create_wizard/static/src/js/*.js",
-        ],
-        "web.assets_qweb": [
-            "fieldservice_sale_order_create_wizard/static/src/xml/*.xml",
-        ],
-    },
 }
--- fieldservice_sale_order_create_wizard/wizard/fsm_create_so_wizard.py
+++ fieldservice_sale_order_create_wizard/wizard/fsm_create_so_wizard.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from odoo import api, fields, models
 
 
@@ -7,11 +5,7 @@ class FsmCreateSoWizard(models.TransientModel):
     _name = "fsm.create.so.wizard"
     _description = "FSM Create Sale Order Wizard"
 
-    location_id = fields.Many2one(
-        "fsm.location",
-        required=True,
-    )
-
+    location_id = fields.Many2one("fsm.location", required=True)
     line_ids = fields.One2many(
         "fsm.create.so.wizard.line",
         "wizard_id",
@@ -20,13 +14,14 @@ class FsmCreateSoWizard(models.TransientModel):
     )
 
     def _prepare_sale_order_vals(self):
-        """Prepare the values to create a Sale Order from the wizard."""
+        self.ensure_one()
+        partner = self.location_id.partner_id
         return {
-            "partner_id": self.location_id.partner_id.id,
-            "pricelist_id": self.location_id.partner_id.property_product_pricelist.id,
+            "partner_id": partner.id,
+            "pricelist_id": partner.property_product_pricelist.id,
             "fsm_location_id": self.location_id.id,
-            "payment_term_id": self.location_id.partner_id.property_payment_term_id.id,
-            "commitment_date": datetime.now(),
+            "payment_term_id": partner.property_payment_term_id.id,
+            "commitment_date": fields.Datetime.now(),
             "order_line": [
                 (
                     0,
@@ -42,35 +37,32 @@ class FsmCreateSoWizard(models.TransientModel):
         }
 
     def action_create_sale_order(self):
-        """Create a Sale Order based on the selected lines."""
-        sale_order = (
-            self.env["sale.order"].sudo().create(self._prepare_sale_order_vals())
+        self.ensure_one()
+        sale_order = self.env["sale.order"].sudo().create(
+            self._prepare_sale_order_vals()
         )
 
         try:
             sale_order.sudo().action_confirm()
         except Exception:
-            # Forces the schedule if the location does not have it set
-            if not self.location_id.fsm_route_id.force_schedule:
-                self.location_id.fsm_route_id.force_schedule = True
+            route = self.location_id.fsm_route_id
+            if route and not route.force_schedule:
+                route.force_schedule = True
             sale_order.sudo().action_confirm()
 
-        fsm_order = sale_order.fsm_order_ids[0]
-        fsm_worker = (
-            self.env["fsm.person"]
-            .sudo()
-            .search(
-                [
-                    ("partner_id", "=", self.env.user.partner_id.id),
-                ],
+        fsm_order = sale_order.fsm_order_ids[:1]
+        if fsm_order:
+            fsm_worker = self.env["fsm.person"].sudo().search(
+                [("partner_id", "=", self.env.user.partner_id.id)],
                 limit=1,
             )
-        )
-        fsm_order.person_id = fsm_worker
+            if fsm_worker:
+                fsm_order.person_id = fsm_worker.id
+
         return {
             "type": "ir.actions.act_window",
             "res_model": "fsm.order",
-            "res_id": fsm_order.id,
+            "res_id": fsm_order.id if fsm_order else False,
             "view_mode": "form",
             "target": "current",
         }
@@ -85,26 +77,25 @@ class FsmCreateSoWizardLine(models.TransientModel):
         required=True,
         ondelete="cascade",
     )
-
     product_id = fields.Many2one(
         "product.product",
         required=True,
-        domain="[('field_service_tracking', 'in', ['sale', 'line'])]",
+        domain=[("field_service_tracking", "in", ["sale", "line"])],
     )
-
     product_uom_id = fields.Many2one(
         "uom.uom",
         string="Unit of Measure",
         required=True,
     )
-
-    quantity = fields.Float(
-        required=True,
-        default=1.0,
+    quantity = fields.Float(required=True, default=1.0)
+    discount = fields.Float(
+        string="Discount (%)",
+        digits="Discount",
+        default=0.0,
     )
-    discount = fields.Float(string="Discount (%)", digits="Discount", default=0.0)
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
-        if self.product_id:
-            self.product_uom_id = self.product_id.uom_id
+        for rec in self:
+            if rec.product_id:
+                rec.product_uom_id = rec.product_id.uom_id
