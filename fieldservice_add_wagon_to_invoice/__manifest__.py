3af95439593f899941b6b7f3c0bd7899859b5610
icefluffy <icefluffy@users.noreply.github.com>
2026-09-08 12:55:25 +0200

    Add manifest file for Field Service Wagon Invoice

---
+++ fieldservice_add_wagon_to_invoice/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2026 TI-Consulting
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Field Service Wagon Invoice Serials",
+    "summary": "Add treated wagon serial numbers to FSM invoice lines",
+    "version": "18.0.1.0.0",
+    "category": "Field Service",
+    "author": "Icefluffy",
+    "license": "AGPL-3",
+    "depends": [
+        "fieldservice",
+        "fieldservice_equipment_stock",
+        "fieldservice_account",
+        "fieldservice_account_analytic",
+        "account",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
