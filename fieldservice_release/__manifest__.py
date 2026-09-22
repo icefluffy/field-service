2669e0230a6cc3a32ef00218e83edfb8ae06532b
icefluffy <icewitty@gmail.com>
2026-09-22 18:57:01 +0200

    template release report

--- fieldservice_release/__manifest__.py
+++ fieldservice_release/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Field Service - Release to service",
     "summary": """Track analytic accounts on Field Service locations
                   and orders""",
-    "version": "18.0.1.1.6",
+    "version": "18.0.1.1.7",
     "category": "Field Service",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/icefluffy/field-service",
